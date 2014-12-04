9f460b13ee3dfaab37264cfbe60b393411169f06
Assaf Gordon <assafgordon@gmail.com>
2014-12-04 00:48:04 -0500

    lobstr: style improvements
    Based on comments from @iml on PR #1409

--- lobstr.rb
+++ lobstr.rb
@@ -1,32 +1,31 @@
-require 'formula'
+require "formula"
 
 class Lobstr < Formula
-  homepage 'http://lobstr.teamerlich.org'
+  homepage "http://lobstr.teamerlich.org"
   url "http://erlichlab.wi.mit.edu/lobSTR/lobSTR-3.0.3.tar.gz"
   sha1 "1f1578e9d1c36eb7268037c977204152f5022361"
-  head 'https://github.com/mgymrek/lobstr-code', :using => :git
 
   option "without-check", "Disable build-time checking (not recommended)"
 
-  if build.head?
+  head do
+    url "https://github.com/mgymrek/lobstr-code.git"
     depends_on :autoconf => :build
     depends_on :automake => :build
   end
+
   depends_on :libtool => :build
-  depends_on 'pkg-config' => :build
+  depends_on "pkg-config" => :build
   depends_on "cppunit" => :build
   depends_on "gsl"
   depends_on "boost"
 
   def install
-    if build.head?
-      system 'sh', './reconf'
-    end
-    system './configure', "--prefix=#{prefix}",
-                          '--disable-dependency-tracking'
-    system 'make'
-    system 'make', 'check' if build.with? "check"
-    system 'make', 'install'
+    system "sh", "./reconf" if build.head?
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking"
+    system "make"
+    system "make", "check" if build.with? "check"
+    system "make", "install"
   end
 
   test do
